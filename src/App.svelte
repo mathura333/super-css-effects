412886a72246fd2641874ce8e5d026880984c615
Mathura Das <mathuradas333@gmail.com>
2021-11-01 01:27:52 +0530

    Added ability to have links

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,54 @@
 <script lang="ts">
-	export let name: string;
+	import Link from './components/Link.svelte'
+	let links = [{
+		id:"1",
+		text:'Google',
+		link: 'https://google.com'
+	},{
+		id:"2",
+		text:'Yahoo',
+		link: 'https://yahoo.com'
+	},{
+		id:"3",
+		text:'Huvile',
+		link: 'https://huvile.com'
+	}]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Super Css Effects</h1>
+	<div class="links">
+		{#each links as link (link.id)}
+		<Link text={link.text} link={link.link}/>
+		{/each}
+	</div>
 </main>
 
 <style>
+	
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		padding: 29px;
+		background: linear-gradient(109.31deg, #FF7455 0%, #F38A29 100%);
+		border-radius: 25px;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		margin:0px;
+		font-family: Merriweather;
+		font-size: 30px;
+		font-style: normal;
+		font-weight: 900;
+		line-height: 33px;
+		letter-spacing: 0em;
+		color: #FFFFFF;
+		letter-spacing: 2px;
+	}
+
+	.links {
+		display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    grid-gap: 1rem;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/components/Link.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let text
+  export let link
+</script>
+
+<a href={link} target="_blank">{text}</a>
+
+<style>
+	a {
+		font-family: IBM Plex Sans;
+font-size: 20px;
+font-style: normal;
+font-weight: 500;
+line-height: 26px;
+letter-spacing: 0em;
+text-align: left;
+color: white;
+text-decoration: none;
+background: #242640;
+border-radius: 10px;
+padding: 0.5rem 1rem;
+text-align: center;
+	}
+
+	a:hover{
+			background: #FFDDB6;
+			color:#242640;
+	}
+</style>
